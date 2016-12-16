5ce6df0ad091b26269bf83d1caead377d8cb4fd3
Quan Dai <qdai@koresystemsgroup.com>
2016-12-16 16:24:23 -0600

    modify build.xml to include NUnit task and point it to the test dll copy NUnit runner to .tools folder modify TCTest.Tests project to add a dummy test.

--- TCTest.Tests/Controllers/HomeControllerTest.cs
+++ TCTest.Tests/Controllers/HomeControllerTest.cs
@@ -1,54 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TCTest;
-using TCTest.Controllers;
+﻿using NUnit.Framework;
 
 namespace TCTest.Tests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
+        [Test]
+        public void MyFirstTest()
         {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
+            var temp = true;
+            Assert.IsTrue(temp);
         }
 
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
+     
     }
 }
